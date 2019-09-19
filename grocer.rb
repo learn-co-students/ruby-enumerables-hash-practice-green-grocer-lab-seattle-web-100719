c055c3ca51b0fbe9366e2977a201fabf1dc6312c
Katrina R <katrina.reinsdorff@gmail.com>
2019-09-19 05:28:45 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,39 @@
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |item|
+    key = item.keys[0]
+    if new_hash[key]
+      new_hash[key][:count] += 1
+    else
+      nested_hash = item[key]
+      nested_hash[:count] = 1
+      new_hash[key] = nested_hash
+    end
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  new_cart = {}
+  cart.each do |k, v|
+    coupon = coupons.find { |coupon| coupon[:item] == k }
+    original_count = v[:count]
+    if_clearance = v[:clearance]
+    initial_price = v[:price]
+
+    if coupon
+      number_coupons = coupons.count(coupon)
+      disc_item = coupon[:item]
+      qty = coupon[:num]
+      total_cost = coupon[:cost]
+      disc_price = total_cost / qty
+      new_cart["#{disc_item} W/COUPON"] = { price: disc_price, clearance: if_clearance, count: qty*number_coupons }
+      new_cart[k] = { price: initial_price, clearance: if_clearance, count: original_count - qty*number_coupons }
+    else
+      new_cart[k] = { price: initial_price, clearance: if_clearance, count: original_count }
+    end
+  end
+  new_cart
 end
 
 def apply_clearance(cart)
