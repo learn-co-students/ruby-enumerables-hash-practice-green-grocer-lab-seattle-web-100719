89ae819e3e8bf3f2bef04d23af39b15e17968cbe
Shawn Huang Fernandes <sfernandes2014@gmail.com>
2019-09-25 05:00:03 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,14 @@
 def consolidate_cart(cart)
-  # code here
+  #get all the unique and turn it into a hash
+  #map through the unique hash and add a new key -----
+            # ---- called count and return the number of occurrences of each key in cart
+ uniqueList = cart.uniq.reduce({}, :update)
+ updatedCart = uniqueList.reduce({}) do |memo, (key, value)|
+   numOfOccurrences = cart.count(key[value])
+   key[value][count] = numOfOccurrences
+ end
+
+ return cartHash
 end
 
 def apply_coupons(cart, coupons)
